2819cefe973d91aede0aaeed8f88aa8cbed1f2ca
James Bachini <james@mcambridge.com>
2020-05-24 23:20:13 +0100

    community page

--- js/test.ts
+++ js/test.ts
@@ -1,9 +1,21 @@
 import { serve } from 'https://deno.land/std/http/server.ts';
-const port : number = 8000;
-const s = serve({ port });
-console.log(`Listening on http://localhost:${port}/`);
-
-for await (const req of s) {
-	req.respond({ body: 'Hello Typescript!' });
+import {
+	acceptWebSocket,
+	isWebSocketCloseEvent,
+	isWebSocketPingEvent,
+} from 'https://deno.land/std/ws/mod.ts';
+const port : string = Deno.args[0] || '8000';
+console.log(`websocket server is running on :${port}`);
+for await (const req of serve(`:${port}`)) {
+	const { conn, r: bufReader, w: bufWriter, headers } = req;
+	const sock = await acceptWebSocket({
+		conn,
+		bufReader,
+		bufWriter,
+		headers,
+	});
+	for await (const ev of sock) {
+		console.log(ev);
+		await sock.send(ev);
+	}
 }
-
