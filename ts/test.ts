64c4dde5960e91b77105b373954e0b631ed6def1
James Bachini <james@mcambridge.com>
2020-05-25 22:45:25 +0100

    cors oak example

--- ts/test.ts
+++ ts/test.ts
@@ -1,12 +1,23 @@
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { Application, Router, send } from 'https://deno.land/x/oak/mod.ts';
+import { oakCors } from 'https://deno.land/x/cors/mod.ts';
 
-Deno.test('Test Example Sync', () => {
-  const a : number = 2 + 2;
-  assertEquals(a, 4);
-});
+const port : number = 8000;
+const router = new Router();
+router
+	.get('/', async (ctx) => {
+		await send(ctx, ctx.request.url.pathname, {
+			root: `${Deno.cwd()}/../`,
+			index: 'index.html',
+		});
+	}) 
+	.get('/user/:uid', async (ctx) => {
+		ctx.response.status = 200
+		ctx.response.body = `hello user ${ctx.params.uid}`;
+  });
 
-Deno.test('Test Example Async', async () => {
-  const a : number = 2 + 2;
-  await new Promise(r => setTimeout(r, 2000)); 
-  if (a !== 4) throw Error('Late night required');
-});
+const app = new Application();
+app.use(oakCors()); // Enable CORS for *
+app.use(router.routes());
+
+console.info(`Listening on port ${port}`);
+await app.listen({ port });
