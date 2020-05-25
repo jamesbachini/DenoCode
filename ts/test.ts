63b7d4fd92237ff74a0e41a31a33c3fe28f42c33
James Bachini <james@mcambridge.com>
2020-05-25 22:00:03 +0100

    testing example

--- ts/test.ts
+++ ts/test.ts
@@ -1,1 +1,12 @@
-import { a1 } from "https://deno.land/x/a1.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+
+Deno.test('Test Example Sync', () => {
+  const a : number = 2 + 2;
+  assertEquals(a, 4);
+});
+
+Deno.test('Test Example Async', async () => {
+  const a : number = 2 + 2;
+  await new Promise(r => setTimeout(r, 2000)); 
+  if (a !== 4) throw Error('Late night required');
+});
