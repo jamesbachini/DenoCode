e6b9fc4d07dd0fc35ee49d6789ce162a1393627d
James Bachini <james@mcambridge.com>
2020-05-25 23:06:30 +0100

    subprocess snippet

--- ts/test.ts
+++ ts/test.ts
@@ -1,23 +1,3 @@
-import { Application, Router, send } from 'https://deno.land/x/oak/mod.ts';
-import { oakCors } from 'https://deno.land/x/cors/mod.ts';
-
-const port : number = 8000;
-const router = new Router();
-router
-	.get('/', async (ctx) => {
-		await send(ctx, ctx.request.url.pathname, {
-			root: `${Deno.cwd()}/../`,
-			index: 'index.html',
-		});
-	}) 
-	.get('/user/:uid', async (ctx) => {
-		ctx.response.status = 200
-		ctx.response.body = `hello user ${ctx.params.uid}`;
-  });
-
-const app = new Application();
-app.use(oakCors()); // Enable CORS for *
-app.use(router.routes());
-
-console.info(`Listening on port ${port}`);
-await app.listen({ port });
+const p = Deno.run({
+  cmd: ["cat", "test.ts"],
+});
